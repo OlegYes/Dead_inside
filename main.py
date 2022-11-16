f27544d73f4446cc91aa2413d87ff373948c85cb
OlegYes <99252117+OlegYes@users.noreply.github.com>
2022-11-17 01:57:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -12,8 +12,5 @@ def print_hi():
     art.tprint("Dead inside.", font="#")
 
 
-    # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi()
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
